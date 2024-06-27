54712ceb276f19a2b32e9c49002111e72f7e7cc4
PACRIM\BS543KY <velan.selvakumar@sg.ey.com>
2024-06-27 22:45:53 +0800

    add basic stepper component to app

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,13 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'app';
+  steplist: StepListType[] = [
+    {
+      stepName:'Basic Details',
+      isComplete:false
+    },
+    {
+      stepName:'Skills',
+      isComplete:false
+    },
+    {
+      stepName:'Experience',
+      isComplete:false
+    }
+  ];
+
+  activeStep: StepListType = this.steplist[0]
+
+  setActiveStep(activeStep:StepListType){
+    this.activeStep = activeStep
+  }
+
+
+}
+
+
+interface StepListType {
+  stepName:string,
+  isComplete:boolean
 }
